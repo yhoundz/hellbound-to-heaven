a6623b98052d19da2ffb43e9e97f5dfdaaf0980c
yhoundz <alexrw008@gmail.com>
2024-05-02 22:33:52 -0500

    demo almost done

--- player/player.gd
+++ player/player.gd
@@ -1,23 +1,138 @@
 extends CharacterBody2D
+class_name Player
 
+enum STATE {
+	IDLE,
+	WALKING,
+	CHARGING,
+	JUMPING,
+	FALLING,
+	WALL_BOUNCE,
+	SLIDING
+	}
 
-@export var speed = 500
-@export var jump_speed = -800
-@export var acceleration = 15
-@export var friction = 6
+@export var speed: int = 45
+@export var jump_speed: int = -200
+@export var acceleration: int = 12
+@export var friction: int = 24
+@export var curr_state: STATE = STATE.IDLE
 
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var jump_timer: Timer = $jump_timer
 
-func get_input(delta):
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = lerp(velocity.x, direction * speed, acceleration * delta)
-	else:
-		velocity.x = lerp(velocity.x, 0.0, friction * delta)
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = jump_speed
+var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+var fall_gravity: int = int(gravity * 1.3)
+var direction: float = 0.0
+var just_jumped: bool = false
+var real_x_velocity: int = 0
+var height_add: int = 0
+
+func _ready() -> void:
+	jump_timer.start()
+	jump_timer.set_paused(true)
+	set_floor_max_angle(0.6981317)
+	set_slide_on_ceiling_enabled(false)
 	
-func _physics_process(delta):
-	velocity.y += gravity * delta
+
+func _physics_process(delta: float) -> void:
+	if curr_state != STATE.FALLING:
+		velocity.y += gravity * delta
+	else:
+		velocity.y += fall_gravity * delta
 	get_input(delta)
 	move_and_slide()
+
+func get_input(delta: float) -> void:
+	direction = Input.get_axis("ui_left", "ui_right")
+	#print(STATE.keys()[curr_state])
+	match curr_state:
+		STATE.IDLE:
+			act_idle(delta)
+		STATE.WALKING:
+			act_walk(delta)
+		STATE.CHARGING:
+			act_charge()
+		STATE.JUMPING:
+			act_jump(delta)
+		STATE.FALLING:
+			act_fall()
+		STATE.WALL_BOUNCE:
+			act_wall_bounce()
+		STATE.SLIDING:
+			act_slide()
+
+func act_idle(delta: float) -> void:
+	if is_on_floor():
+		velocity.y = 0
+		if direction:
+			curr_state = STATE.WALKING
+		else:
+			velocity.x = lerp(velocity.x, 0.0, friction * delta)
+		if Input.is_action_just_pressed("ui_accept"):
+				curr_state = STATE.CHARGING
+	if velocity.y > 0:
+		curr_state = STATE.FALLING
+
+func act_walk(delta: float) -> void:
+	if is_on_floor():
+		velocity.y = 0
+		if direction:
+			velocity.x = lerp(velocity.x, direction * speed, acceleration * delta)
+		else:
+			curr_state = STATE.IDLE
+		if Input.is_action_just_pressed("ui_accept"):
+			curr_state = STATE.CHARGING
+	if velocity.y > 0:
+		curr_state = STATE.FALLING
+
+func act_charge() -> void:
+	velocity.x = 0
+	if Input.is_action_just_released("ui_accept") and is_on_floor():
+		curr_state = STATE.JUMPING
+	charge_jump()
+
+func act_jump(delta: float) -> void:
+	if not just_jumped:
+		try_jump(direction, delta)
+		just_jumped = true
+	if is_on_ceiling_only():
+		velocity.y = 0
+		curr_state = STATE.FALLING
+	if velocity.y > 0 and just_jumped:
+		curr_state = STATE.FALLING
+	else:
+		if is_on_wall_only():
+			var bounce_velocity: int = -real_x_velocity
+			bounce_velocity *= (get_real_velocity().y/(jump_speed - height_add))
+			velocity.x = bounce_velocity
+			curr_state = STATE.WALL_BOUNCE
+
+func act_fall() -> void:
+	just_jumped = false
+	if is_on_floor():
+		curr_state = STATE.IDLE
+
+func act_wall_bounce() -> void:
+	if is_on_floor():
+		curr_state = STATE.IDLE
+	if velocity.y > 0:
+		curr_state = STATE.FALLING
+
+func act_slide() -> void:
+	pass
+
+func charge_jump() -> void:
+	if jump_timer.is_paused():
+		jump_timer.set_paused(false)
+		jump_timer.start()
+
+func try_jump(direction: float, delta: float) -> void:
+	jump_timer.set_paused(true)
+	
+	real_x_velocity = int(180 * direction)
+	height_add = int(280 * (jump_timer.wait_time - jump_timer.time_left))
+	velocity.y = jump_speed - height_add
+	velocity.x = real_x_velocity
+
+func _on_jump_timer_timeout() -> void:
+	if curr_state == STATE.CHARGING:
+		curr_state = STATE.JUMPING
---
+++ world/mehanics/portal.gd
@@ -0,0 +1,15 @@
+extends Node2D
+class_name Portal
+
+@onready var entrance: Area2D = $entrance
+@onready var exit: Area2D = $exit
+
+func _ready() -> void:
+	pass
+
+func _process(delta):
+	pass
+
+func _on_entrance_body_entered(body: Node2D) -> void:
+	if body is Player:
+		body.global_position = exit.global_position
