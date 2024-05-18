3a10406078ad04b13c0a53781bd911058dc280d7
yhoundz <alexrw008@gmail.com>
2024-05-18 15:02:19 -0500

    new

--- player/player.gd
+++ player/player.gd
@@ -108,7 +108,7 @@ func act_jump(delta: float) -> void:
 	else:
 		if is_on_wall_only():
 			var bounce_velocity: int = -jump_vector_x
-			bounce_velocity *= 0.4 * (get_real_velocity().y/(jump_speed - height_add))
+			bounce_velocity *= 0.6 * (get_real_velocity().y/(jump_speed - height_add))
 			velocity.x = bounce_velocity
 			curr_state = STATE.WALL_BOUNCE
 
@@ -119,7 +119,7 @@ func act_fall() -> void:
 	else:
 		if is_on_wall_only():
 			var bounce_velocity: int = -jump_vector_x
-			bounce_velocity *= 0.4 * (-get_real_velocity().y/(jump_speed - height_add))
+			bounce_velocity *= 0.6 * (-get_real_velocity().y/(jump_speed - height_add))
 			velocity.x = bounce_velocity
 			curr_state = STATE.WALL_BOUNCE
 
