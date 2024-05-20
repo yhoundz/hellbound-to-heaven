8eaf0cdc6fc40f57d3194f015d4975df412f01e4
yhoundz <alexrw008@gmail.com>
2024-05-19 22:18:59 -0500

    added sfx and music

--- player/player.gd
+++ player/player.gd
@@ -17,6 +17,10 @@ enum STATE {
 @export var curr_state: STATE = STATE.IDLE
 
 @onready var jump_timer: Timer = $jump_timer
+@onready var sfx_jump: AudioStreamPlayer2D = $sfx_jump
+@onready var sfx_walk = $sfx_walk
+@onready var sfx_land_bounce = $sfx_land_bounce
+@onready var sfx_charge = $sfx_charge
 
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 var fall_gravity: int = int(gravity * 1.3)
@@ -25,6 +29,7 @@ var just_jumped: bool = false
 var height_add: int = 0
 var jump_vector_x: int = 0
 var velocity_modifier: int = 0
+var sfx_just_played: bool = false
 
 signal hitboxDirection(rp)
 
@@ -32,7 +37,6 @@ func _ready() -> void:
 	jump_timer.start()
 	jump_timer.set_paused(true)
 	set_slide_on_ceiling_enabled(false)
-	
 
 func _physics_process(delta: float) -> void:
 	
@@ -100,6 +104,9 @@ func act_idle(delta: float) -> void:
 		curr_state = STATE.FALLING
 
 func act_walk(delta: float) -> void:
+	if not sfx_just_played:
+		sfx_walk.play()
+		sfx_just_played = true
 	if is_on_floor():
 		velocity.y = 0
 		if direction == -1:
@@ -109,50 +116,77 @@ func act_walk(delta: float) -> void:
 		if direction:
 			velocity.x = lerp(velocity.x, direction * speed, acceleration * delta)
 		else:
+			sfx_just_played = false
+			sfx_walk.stop()
 			curr_state = STATE.IDLE
 		if Input.is_action_just_pressed("ui_accept"):
+			sfx_walk.stop()
+			sfx_just_played = false
 			curr_state = STATE.CHARGING
 	if velocity.y > 0:
+		sfx_just_played = false
 		curr_state = STATE.FALLING
 
 func act_charge() -> void:
+	if not sfx_just_played:
+		sfx_charge.play()
+		sfx_just_played = true
 	velocity.x = 0
 	if Input.is_action_just_released("ui_accept") and is_on_floor():
+		sfx_just_played = false
 		curr_state = STATE.JUMPING
 	charge_jump()
 
 func act_jump(delta: float) -> void:
+	if not sfx_just_played:
+		sfx_jump.play()
+		sfx_just_played = true
 	if not just_jumped:
 		try_jump(direction, delta)
 		just_jumped = true
 	if is_on_ceiling_only():
 		velocity.y = 0
+		if not sfx_just_played:
+			sfx_jump.play()
+			sfx_just_played = true
+		sfx_just_played = false
 		curr_state = STATE.FALLING
 	if velocity.y > 0 and just_jumped:
+		sfx_just_played = false
 		curr_state = STATE.FALLING
 	else:
 		if is_on_wall_only():
 			var bounce_velocity: int = -jump_vector_x
 			bounce_velocity *= 0.6 * (get_real_velocity().y/(jump_speed - height_add))
 			velocity.x = bounce_velocity
+			sfx_just_played = false
 			curr_state = STATE.WALL_BOUNCE
 
 func act_fall() -> void:
 	just_jumped = false
 	if is_on_floor():
+		if not sfx_just_played:
+			sfx_land_bounce.play()
+			sfx_just_played = false
 		curr_state = STATE.IDLE
 	else:
 		if is_on_wall_only():
 			var bounce_velocity: int = -jump_vector_x
 			bounce_velocity *= 0.6 * (-get_real_velocity().y/(jump_speed - height_add))
 			velocity.x = bounce_velocity
+			sfx_just_played = false
 			curr_state = STATE.WALL_BOUNCE
 
 func act_wall_bounce():
+	if not sfx_just_played:
+		sfx_land_bounce.play()
+		sfx_just_played = true
 	if is_on_floor():
 		velocity.x = 0
+		sfx_just_played = false
 		curr_state = STATE.IDLE
 	if velocity.y > 0:
+		sfx_just_played = false
 		curr_state = STATE.FALLING
 
 func charge_jump() -> void:
@@ -171,7 +205,3 @@ func try_jump(direction: float, delta: float) -> void:
 func _on_jump_timer_timeout() -> void:
 	if curr_state == STATE.CHARGING:
 		curr_state = STATE.JUMPING
-
-
-func _on_entrance_body_entered(body):
-	pass # Replace with function body.
